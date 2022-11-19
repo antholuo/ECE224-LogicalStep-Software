e12794cf8cab91b26eef026839468e9af51321e9
Anthony Luo <anthonyluo100@gmail.com>
2022-11-18 20:36:24 -0500

    LAB2_CLI

---
+++ LAB2_CLI_Testing/obj/default/hello_world.d
@@ -0,0 +1,61 @@
+obj/default/hello_world.o: hello_world.c ../LAB2_CLI_Testing_bsp/system.h \
+ ../LAB2_CLI_Testing_bsp/linker.h \
+ ../LAB2_CLI_Testing_bsp//HAL/inc/sys/alt_alarm.h \
+ ../LAB2_CLI_Testing_bsp//HAL/inc/sys/alt_llist.h \
+ ../LAB2_CLI_Testing_bsp//HAL/inc/alt_types.h \
+ ../LAB2_CLI_Testing_bsp//HAL/inc/priv/alt_alarm.h \
+ ../LAB2_CLI_Testing_bsp//HAL/inc/io.h \
+ ../LAB2_CLI_Testing_bsp//HAL/inc/alt_types.h fatfs.h ff.h ffconf.h \
+ fatfs_config.h fatfs_types.h diskio.h fatfs_diskio.h monitor.h uart.h \
+ ../LAB2_CLI_Testing_bsp//drivers/inc/altera_up_avalon_audio.h \
+ ../LAB2_CLI_Testing_bsp//HAL/inc/sys/alt_dev.h \
+ ../LAB2_CLI_Testing_bsp//HAL/inc/sys/alt_llist.h \
+ ../LAB2_CLI_Testing_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../LAB2_CLI_Testing_bsp//drivers/inc/altera_up_avalon_audio_and_video_config.h \
+ ../LAB2_CLI_Testing_bsp//drivers/inc/altera_up_avalon_audio_and_video_config_regs.h
+
+../LAB2_CLI_Testing_bsp/system.h:
+
+../LAB2_CLI_Testing_bsp/linker.h:
+
+../LAB2_CLI_Testing_bsp//HAL/inc/sys/alt_alarm.h:
+
+../LAB2_CLI_Testing_bsp//HAL/inc/sys/alt_llist.h:
+
+../LAB2_CLI_Testing_bsp//HAL/inc/alt_types.h:
+
+../LAB2_CLI_Testing_bsp//HAL/inc/priv/alt_alarm.h:
+
+../LAB2_CLI_Testing_bsp//HAL/inc/io.h:
+
+../LAB2_CLI_Testing_bsp//HAL/inc/alt_types.h:
+
+fatfs.h:
+
+ff.h:
+
+ffconf.h:
+
+fatfs_config.h:
+
+fatfs_types.h:
+
+diskio.h:
+
+fatfs_diskio.h:
+
+monitor.h:
+
+uart.h:
+
+../LAB2_CLI_Testing_bsp//drivers/inc/altera_up_avalon_audio.h:
+
+../LAB2_CLI_Testing_bsp//HAL/inc/sys/alt_dev.h:
+
+../LAB2_CLI_Testing_bsp//HAL/inc/sys/alt_llist.h:
+
+../LAB2_CLI_Testing_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../LAB2_CLI_Testing_bsp//drivers/inc/altera_up_avalon_audio_and_video_config.h:
+
+../LAB2_CLI_Testing_bsp//drivers/inc/altera_up_avalon_audio_and_video_config_regs.h:
